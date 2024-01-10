049551c6f90d0b95fe7ac67d01a1a1f0ead9b18f
Tom Dent <tomdent991@hotmail.com>
2024-01-10 16:03:38 +0100

    details page linked to db

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,14 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+// import { db_places } from "../../../../lib/db_places";
+// import { db_comments } from "../../../../lib/db_comments";
+import Place from "../../../../db/models/Place";
+import dbConnect from "../../../../db/connect";
 
-export default function handler(request, response) {
-  const { id } = request.query;
+export default async function handler(req, res) {
+  const { id } = req.query;
 
-  if (!id) {
-    return;
+  await dbConnect();
+  if (req.method === `GET`) {
+    const place = await Place.findById(id);
+    return res.status(200).json({ place: place});
   }
-
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json({ place: place, comments: comments });
 }
